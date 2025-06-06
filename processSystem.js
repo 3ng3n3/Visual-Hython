e39f43452d1881ef2e1ec420a359c689f30359e3
3ng3n3 <165216857+3ng3n3@users.noreply.github.com>
2025-06-05 21:41:06 -0400

    Update processSystem.js

--- processSystem.js
+++ processSystem.js
@@ -430,7 +430,7 @@ DisplayCompiler.addEventListener("click", () => {
   DisplayCompiler.style.left = "-1000px";
 })
 const RequestsCode = (code) => {
-     fetch('http://127.0.0.1:5000', {
+     fetch('http://servidorforadoarhaha', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
